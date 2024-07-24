a76905cbfc97287be75af7c8149bc5f1e351f835
omiras <oscar.miras@gmail.com>
2024-07-24 20:23:28 +0200

    error handling

---
+++ exceptions-handling/defensive-programming/array-methods.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, -7];
+
+// Queremos quedarnos solo con los números positivos
+console.log(numbers.filter("PEPE"));
+
+console.log("Sigo haciendo cosas");
---
+++ exceptions-handling/defensive-programming/concatenate-two-strings.js
@@ -0,0 +1,18 @@
+function concatenateStrings(s1, s2) {
+    console.log(typeof s2);
+    if (!(typeof s1 == 'string') || !(typeof s2 == 'string')) {
+        // el uso de 'throw' termina inmediatamente la ejecución de la función
+        throw new Error("Both arguments must be strings.")
+    }
+
+    return `${s1} ${s2}`
+}
+
+const person = {
+    fistName: "John",
+    lastName: "Walker",
+    age: 24,
+    hobbies: ['walk', 'hunt', 'drink alcohol']
+}
+
+console.log(concatenateStrings(person.fistName, person.hobbies));
