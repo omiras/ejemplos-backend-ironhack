a14114d1b1078d8a7c8629c515929e954d449eea
omiras <oscar.miras@gmail.com>
2024-08-06 21:07:06 +0200

    obvious man

--- mongoose/app-dungeon-dragons.js
+++ mongoose/app-dungeon-dragons.js
@@ -7,11 +7,15 @@ async function main() {
 
     // 1. Crear el Schema
 
-    // 2. Asociarlo al Model
+    // 2. Asociarlo al Model . A la colección donde guardar los personajes le llamaremos 'Character'
 
     // 3. Crear al menos 2 documentos.
     // Aragon que tiene 5 de poder de ataque, 12 de inteligencia , 10 de puntos de vida. Su nivel de personaje es 5
     // Gollum que tiene 2 de poder de ataque, 3 de inteligencia , 5 de puntos de vida. NO establecemos su nivel de persoje (por defecto se debería rellenar con un 0)
+
+    // Corregir en 20 min
+
+    // Comprobad que habéis creado los documentos en la base de datos
 
 
 
