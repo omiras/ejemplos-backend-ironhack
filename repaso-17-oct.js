6d6226d5bef91a31f70c1a99ea994767894ea7f0
omiras <oscar.miras@gmail.com>
2024-10-17 19:28:50 +0200

    Co-authored-by: Andrii Sharmar <SharmarAndrey@users.noreply.github.com>

--- repaso-17-oct.js
+++ repaso-17-oct.js
@@ -1,7 +1,7 @@
 
 /**
  * Task
-You are given a arrat of objetos containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+You are given a array of objetos containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
 
 Note: the scores will always be unique (so no duplicate values)
  */
